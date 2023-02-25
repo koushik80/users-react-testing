cbf6fc0a87366751e6ea749cda2799c17e3202a1
Koushik Dey <koushik.dey@live.com>
2023-02-25 14:09:19 +0200

    written code for form and list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import { useState } from 'react';
+import UserForm from './components/UserForm';
+import UserList from './components/UserList';
 
 const App = () => {
+  const [users, setUsers] = useState([]);
+
+  const onUserAdd = (user) => {
+    setUsers([...users, user]);
+  };
+
   return (
     <div>
-      
+      <UserForm onUserAdd={onUserAdd} />
+      <hr />
+      <UserList users={users} />
     </div>
-  )
+  );
 }
 
 export default App;
